063f0cfbc1070ea274f4d52b1f56d89c998dbdff
panwar67 <shubhampanwar67@yahoo.com>
2017-04-14 00:14:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sparsh23/car_ingicons/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sparsh23.car_ingicons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
